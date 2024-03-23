3648677d2cdff00bf08625e8d81a67682a6bc91c
MIKI201487 <149659815+MIKI201487@users.noreply.github.com>
2024-03-24 09:32:50 +1100

    Update execute.js

--- execute.js
+++ execute.js
@@ -3,6 +3,7 @@ var password = "lone_tomato";
 function passcheck () {
 if(document.getElementById('lone_tomato').value != password) {
 alert('Wrong Tomato');
+return false;
 }
 
   if(document.getElementById('lone_tomato').value == password) {
